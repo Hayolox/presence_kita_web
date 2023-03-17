d4db7b362f368688de0248d18a812f9a6fa9c769
hayolox <akiklii72@gmail.com>
2023-03-17 22:57:57 +0700

    pratikum

---
+++ database/migrations/2023_03_17_155426_create_classroomspratikums_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classroomspratikums', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('subject_course_code');
+            $table->foreign('subject_course_code')->references('course_code')->on('subjects');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classroomspratikums');
+    }
+};
---
+++ database/migrations/2023_03_17_155427_create_sessionpratikums_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sessionpratikums', function (Blueprint $table) {
+            $table->id();
+            $table->string('QrCode', 20);
+            $table->string("title", 100);
+            $table->time('start');
+            $table->time('finish');
+            $table->date('date');
+            $table->string('lecturer_nip');
+            $table->foreign('lecturer_nip')->references('nip')->on('lecturers');
+            $table->unsignedBigInteger('semester_id');
+            $table->foreign('semester_id')->references('id')->on('semesters');
+            $table->unsignedBigInteger('classroomspratikum_id');
+            $table->foreign('classroomspratikum_id')->references('id')->on('classroomspratikums');
+            $table->integer("year");
+            $table->unsignedBigInteger('room_id');
+            $table->foreign('room_id')->references('id')->on('rooms');
+            $table->boolean('geolocation');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sessionpratikums');
+    }
+};
